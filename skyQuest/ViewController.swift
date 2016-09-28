e853169b55f644114162c16188bc630afc976c4b
ferrufino <gustavo.fdlf@gmail.com>
2016-09-28 09:56:34 -0500

    adds project

---
+++ skyQuest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  skyQuest
+//
+//  Created by Gustavo Ferrufino De La Fuente on 9/28/16.
+//  Copyright © 2016 itesm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
