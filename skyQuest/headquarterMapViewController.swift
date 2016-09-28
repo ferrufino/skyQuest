de5cc5ae92acc3b995f2b0cacc98ac87b8b04f18
ferrufino <gustavo.fdlf@gmail.com>
2016-09-28 16:27:09 -0500

    adds headquarter file & skyQuest nav btns

---
+++ skyQuest/headquarterMapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  headquarterMapViewController.swift
+//  skyQuest
+//
+//  Created by Gustavo Ferrufino De La Fuente on 9/28/16.
+//  Copyright © 2016 itesm. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class headquarterMapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
